a8ab3063b5d43d07c93e1e2221d709d933477ddf
Stephen Crockett <crockett95@gmail.com>
2014-03-06 20:24:33 -0600

    Empty

--- app/scripts/main.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    'use strict';
-    var dropDead = new Date('2014-03-11T14:00:00+0000');
-    $('#countdown').countdown({
-        until: dropDead,
-        compact: true,
-        layout: '{dnn}{dl}&nbsp;{hnn}{sep}{mnn}{sep}{snn}'
-    });
-})(jQuery);
